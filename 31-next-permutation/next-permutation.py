d0511c81c42a41d5b1e51e0a728a4111570c7906
Tarun <tarunam553@gmail.com>
2026-08-26 08:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (67.05%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        n = len(nums)
+        pivot = n - 2
+
+        while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
+            pivot -= 1
+
+        if pivot >= 0:
+            successor = n - 1
+
+            while nums[successor] <= nums[pivot]:
+                successor -= 1
+
+            nums[pivot], nums[successor] = (
+                nums[successor],
+                nums[pivot],
+            )
+
+        left = pivot + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
